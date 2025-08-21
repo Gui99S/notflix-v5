7cf21513a5cc544c355433ac8870467df3c1cd2c
Guilherme Santos <guilherme99ssilva@hotmail.com>
2025-08-21 19:24:55 -0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import MovieDetails from './pages/MovieDetails.jsx';
 import Navbar from './components/Layout/Navbar.jsx';
